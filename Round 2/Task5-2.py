3da216069709a8e57d37e72ae1b7f03911205cc3
Unknown <imran.pavel@gmail.com>
2018-09-23 18:56:26 +0300

    Round 2 Task 5 2

---
+++ Round 2/Task5-2.py
@@ -0,0 +1,15 @@
+
+def checkTickets(winningTicket, tickets):
+    winningTicket.sort()
+    print("Correct numbers: " + " ".join(str(digit) for digit in winningTicket))
+    for ticket in tickets:
+        ticket.sort()
+        matchedDigits = []
+        for digit in ticket:
+            if digit in winningTicket:
+                matchedDigits.append(digit)
+        if len(matchedDigits) > 0:
+            print("Ticket: " + " ".join(str(digit) for digit in ticket) + " (" + str(len(matchedDigits)) + " correct: " + " ".join(str(digit) for digit in matchedDigits) + ")")
+        else:
+            print("Ticket: " + " ".join(str(digit) for digit in ticket) + " (0 correct)")
+        
