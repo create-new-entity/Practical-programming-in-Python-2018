18c959ccda830d85f7dd3f039e0e60a987a75ed3
Unknown <imran.pavel@gmail.com>
2018-09-24 04:25:50 +0300

    Round 2 Task 7 2

---
+++ Round 2/Task7-2.py
@@ -0,0 +1,44 @@
+import sys
+
+encryptD = {}
+decryptD = {}
+
+
+def setDictionaries(cipherAlphabet):
+    i = 'a'
+    j = 0
+    while True:
+        encryptD[i] = cipherAlphabet[j]
+        decryptD[cipherAlphabet[j]] = i
+        if i == 'z':
+            break
+        i = chr(ord(i)+1)
+        j += 1
+        
+setDictionaries(sys.argv[1])
+
+
+def enOrdeCrypt(dict, inputStr):
+    outputStr = ""
+    for c in inputStr:
+        if 'a' <= c <= 'z':
+            outputStr += dict[c]
+        elif 'A' <= c <= 'Z':
+            outputStr += dict[c.lower()].upper()
+        else:
+            outputStr += c
+    return outputStr
+
+
+while True:
+    command = input("Give a command: ")
+    if command.find("encrypt ") != -1:
+        command = command.partition("encrypt ")
+        print(enOrdeCrypt(encryptD, command[2]))
+    elif command.find("decrypt ") != -1:
+        command = command.partition("decrypt ")
+        print(enOrdeCrypt(decryptD, command[2]))
+    elif command == "quit":
+       break
+    else:
+        print("Illegal command!")
